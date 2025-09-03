4d4fe5569437c12f04a0282395fc328ad0bcace8
mambea-solo <solomoni17@yahoo.com>
2025-09-03 12:32:02 +0200

    Add files via upload

---
+++ Codes/Chapter 5.5/StrapdownRegressionLayer.m
@@ -0,0 +1,82 @@
+classdef StrapdownRegressionLayer < nnet.layer.RegressionLayer
+    % regression layer to evaluate rotation rates estimates against known
+    % attitude values
+    % does account for the fact, that the initial attitude is unknown, i.e.
+    % the loss functions conducts internally some kind of coarse alignment
+    properties 
+        f_imu; % assumed IMU sample rate 
+    end
+
+    methods
+        function layer = StrapdownRegressionLayer(name)
+            % Set layer name.
+            layer.Name = name;
+            layer.f_imu=200; % assumed IMU sample rate [note: for NN training is actually irrelevant; it is just kept here for didactic purposes]
+            
+            % Set layer description.
+            layer.Description = 'using the NN output, an 1D attitude strapdown computation is performed and is matched to know attitude values';
+        end
+        
+        function loss = forwardLoss(layer,Y, T)
+            % Y ... calibrated rotation ratesa in [rad/s] as output by the NN; 
+            %       index scheme Y(x,y,z): x ... 1, y ... trajectory index,
+            %       z ... time epoch index
+      
+            % T ... reference attitude values [rad]
+            
+
+            
+            %integrate rotation rates into attiude, note: '3' is the time
+            %dimension of the input data
+            %note: the integration starts here at 0 deg, but actually the
+            %intial values is irrelevant as psi0 accounts for any offset
+            att = cumsum(Y,3)/layer.f_imu; 
+
+            % poor mans coarse alignement computes the initial angular
+            % value as the mean between the true angular values and the
+            % ones from the strapdown computation (=cumsum)
+            psi0= abs(mean( att-T ,3)); % note: 3 indicates the dimension along the mean is computed
+                                   % third index counts the different test
+                                   % data sets
+            
+            % apply now the coarse alignment to the result from the
+            % strapdown computation
+            % since we cover multiple training sets in one step, care must
+            % be taken to ensure proper indexing
+            att = att + repmat( psi0, [1, 1, size(att,3)]);
+
+            % compute the L2 norm between computed and reference attitude
+            SSE = sum((att-T).^2,3); %L2-attitude norm for each trajectory
+            SSE = sum(SSE); % sum over all trajectories
+            
+            % return loss function
+            loss=SSE;
+
+        end
+
+         function dLdY = backwardLoss(layer,Y,T)
+            % dLdY = backwardLoss(layer, Y, T) returns the derivative of
+            % the loss function L, with respect to the predictions Y.
+            % L= SSE equation
+            %
+            % in our case we need to compute the derivative of the L2
+            % attitude norm w.r.t. to the rotation rates as produced by the
+            % NN.
+            %
+            % Y, T ... see definition of forwardLoss
+            
+
+            %integrate rotation rates into attiude, note: '3' is the time
+            %dimension of the input data
+            att = cumsum(Y,3)/layer.f_imu;
+            
+            % apply coarse alignment also the purpose of computing the first derivate of the loss function 
+            psi0=abs(mean( att-T,3));
+            att = att + repmat( psi0, [1, 1, size(att,3)]);
+            
+            % compute first derivative (see handwritten notes)
+            dLdY = cumsum(2*(att-T),3,'reverse')/layer.f_imu^2; 
+
+        end
+    end
+end
---
+++ Codes/Chapter 5.5/sim_fixed_pos.m
@@ -0,0 +1,115 @@
+function  simulation = sim_fixed_pos(BW, ~) 
+
+% clear
+% close all
+% clc
+
+%% simulation szenario:
+% An IMU mounted on a turntable which moves at 10 deg steps around the z-axis
+% while resting for 10 seconds. The rotation starts with zero postion means x-axis points tword north direction and
+% going clock wise while making stops every 10 deg for couple of seconds
+
+% load simulated data by the IMU simulator of Mohamed Bochkati
+
+load sim_static_pos_60sec_stops.mat
+t=OBS.time;
+
+%1. plot simulation profile
+figure
+subplot(2,1,1)
+plot(t,OBS.attitude_GT.psi)
+grid on
+ylabel('heading [deg]')
+subplot(2,1,2)
+plot(t,OBS.attitude_GT.psi_dot)
+xlabel('time [sec]')
+ylabel('heading rates [deg/s]')
+
+%2. IMU erro model
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% spec for @ian
+%--------------------------------------------
+
+Xsens_spec.gyro_cst   = [700 ,    0.15];      % only bias [deg/h)] and scale factor [%]
+Xsens_spec.gyro_rand  = [1   , 10, 0.5];      % random erros: ARW (angle white noise) [deg/sqrt(h)] and bias instabilty modeld [deg/h] as GM-1st-order and corr. time [hour]
+
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% spec for @elsie
+%--------------------------------------------
+
+iFOG_spec.gyro_cst    = [1,       0.01];      % only bias [deg/h] and scale factor [%]
+iFOG_spec.gyro_rand   = [0.15,    0.07];      % random erros: ARW (angle white noise) [deg/sqrt(h)] and bias instabilty modeld [deg/h]
+
+
+%% convert spec units for m/s² or rad/s
+% constant error
+
+bias_gyro = Xsens_spec.gyro_cst(1) * (pi/(180*3600));    % [rad/s];
+SF_gyro   = Xsens_spec.gyro_cst(2) * 0.01;
+
+% random error
+BW  = 200;        % assuming 200 Hz of IMU-band width
+ARW = (Xsens_spec.gyro_rand(1)*60 *sqrt(BW) * pi)/(180*3600); % [rad/s]
+
+
+% define random vector for both wx and wy
+
+WNx  =randn(length(t),1);            % random vector to simulate imu spec
+WNy  =randn(length(t),1);            % random vector to simulate imu spec
+WNz  =randn(length(t),1);            % random vector to simulate imu spec
+
+
+%% Normalization of error free signal and generate noisy signals 
+% for north finding technique assmuning same constant error for all three gyro axis
+% wx_free         = (OBS.omega_ib_b_GT(:,1) - min(OBS.omega_ib_b_GT(:,1))) / range(OBS.omega_ib_b_GT(:,1));
+% wy_free         = (OBS.omega_ib_b_GT(:,2) - min(OBS.omega_ib_b_GT(:,2))) / range(OBS.omega_ib_b_GT(:,2));
+% wz_free         = (OBS.omega_ib_b_GT(:,3) - min(OBS.omega_ib_b_GT(:,3))) / range(OBS.omega_ib_b_GT(:,3));
+% 
+% wx_noisy        = wx_free * (1+SF_gyro) + bias_gyro + ARW * WNx;
+% wy_noisy        = wy_free * (1+SF_gyro) + bias_gyro + ARW * WNy;
+% wz_noisy        = wz_free * (1+SF_gyro) + bias_gyro + ARW * WNz;
+%% generate noisy signals for north finding technique assmuning same constant error for all three gyro axis
+
+wx_noisy    = OBS.omega_ib_b_GT(:,1) * (1+SF_gyro) + bias_gyro + ARW * WNx;
+wy_noisy    = OBS.omega_ib_b_GT(:,2) * (1+SF_gyro) + bias_gyro + ARW * WNy;
+wz_noisy    = OBS.omega_ib_b_GT(:,3) * (1+SF_gyro) + bias_gyro + ARW * WNz;
+
+figure
+subplot(3,1,1)
+plot( t, wx_noisy)
+hold on
+plot( t,OBS.omega_ib_b_GT(:,1))
+ylabel('w_x [rad/sec]')
+subplot(3,1,2)
+plot( t, wy_noisy)
+hold on
+plot( t,OBS.omega_ib_b_GT(:,2))
+ylabel('w_y [rad/sec]')
+subplot(3,1,3)
+plot( t, wz_noisy)
+hold on
+plot( t,OBS.omega_ib_b_GT(:,3))
+xlabel('time [sec]')
+ylabel('w_z [rad/sec]')
+
+%% collected data from simulation  
+simulation.gyro_x_noisy     = wx_noisy;
+simulation.gyro_y_noisy     = wy_noisy;
+simulation.gyro_z_noisy     = wz_noisy;
+
+simulation.gyro_x_ref       = OBS.omega_ib_b_GT(:,1); 
+simulation.gyro_y_ref       = OBS.omega_ib_b_GT(:,2); 
+simulation.gyro_z_ref       = OBS.omega_ib_b_GT(:,3); 
+
+simulation.bias_gyro        = bias_gyro;
+simulation.SF_gyro          = SF_gyro;
+simulation.ARW              = ARW;
+
+simulation.psi              = OBS.attitude_GT.psi*pi/180;
+simulation.psi_dot          = OBS.attitude_GT.psi_dot;
+
+simulation.t                = t;
+
+end
+
+
